2e4866757944da08804af717c888e4b0af0b2b91
KolyaVojtekh <kolyavojteh2@gmail.com>
2023-11-05 15:35:47 +0200

    added handlers for SensorManager

--- components/DistributedSensorSystemClient/DistributedSensorSystemClient.cpp
+++ components/DistributedSensorSystemClient/DistributedSensorSystemClient.cpp
@@ -16,11 +16,15 @@ DistributedSensorSystemClient::DistributedSensorSystemClient()
 
 void DistributedSensorSystemClient::initHandlers()
 {
-    // MeshDataExchangeModule::m_mechanismFromExternalIP.addHandler("aliveNodeRequest", DSSProtocolHandler::aliveNodeRequestHandler);
-
     MeshDataExchangeModule::m_mechanismPacketHandlers.addHandler("bootstrapInRoot", DSSProtocolHandler::bootstrapRootHandler);
     MeshDataExchangeModule::m_mechanismPacketHandlers.addHandler("aliveNodeRequest", DSSProtocolHandler::aliveNodeRequestHandler);
     MeshDataExchangeModule::m_mechanismPacketHandlers.addHandler("chronoUpdateResponse", DSSProtocolHandler::chronoUpdateResponseHandler);
     MeshDataExchangeModule::m_mechanismPacketHandlers.addHandler("getConfigRequest", DSSProtocolHandler::getConfigRequestHandler);
     MeshDataExchangeModule::m_mechanismPacketHandlers.addHandler("setConfigRequest", DSSProtocolHandler::setConfigRequestHandler);
+
+    MeshDataExchangeModule::m_mechanismPacketHandlers.addHandler("getSensorsPacketRequest", DSSProtocolHandler::getSensorsPacketRequestHandler);
+    MeshDataExchangeModule::m_mechanismPacketHandlers.addHandler("getSensorHeaderPacketRequest", DSSProtocolHandler::getSensorHeaderPacketRequestHandler);
+    MeshDataExchangeModule::m_mechanismPacketHandlers.addHandler("getSensorDataPacketRequest", DSSProtocolHandler::getSensorDataPacketRequestHandler);
+    MeshDataExchangeModule::m_mechanismPacketHandlers.addHandler("clearSensorDataRequest", DSSProtocolHandler::clearSensorDataRequestHandler);
+    MeshDataExchangeModule::m_mechanismPacketHandlers.addHandler("setSensorReadingMode", DSSProtocolHandler::setSensorReadingModeRequestHandler);
 }
